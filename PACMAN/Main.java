2afecf10b123a150fe2fe6ea74502669d5ae51ce
Amar Parkash Singh <asin579@aucklanduni.ac.nz>
2018-03-28 02:28:24 +1300

    created window, added multiple buttons, scenes now changing scenes

--- PACMAN/Main.java
+++ PACMAN/Main.java
@@ -4,20 +4,54 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
+
+	Scene homeScene;
+	Scene helpScene;
+	Scene singlePlayerScene;
+	Scene multiPlayerScene;
+	Scene highScoreScene;
+
+	Button buttonSinglePlayer;
+	Button buttonMultiPlayer;
+	Button buttonHelp;
+	Button buttonHighScore;
+	Button buttonHomeScreen; //helper button
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1024,768);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage){
+
+		primaryStage.setTitle("PAC-MAN Dragon Edition");
+
+		buttonSinglePlayer = new Button("Single Player");
+		buttonSinglePlayer.setOnAction(e -> primaryStage.setScene(singlePlayerScene));
+		buttonMultiPlayer = new Button("Multi-Player");
+		buttonHelp = new Button("Help");
+		buttonHighScore = new Button("High Score");
+
+		buttonHomeScreen = new Button("Main menu");
+		buttonHomeScreen.setOnAction(e -> primaryStage.setScene(homeScene));
+
+		VBox homeLayout = new VBox(20);
+		homeLayout.getChildren().add(buttonSinglePlayer);
+		homeLayout.getChildren().add(buttonMultiPlayer);
+		homeLayout.getChildren().add(buttonHelp);
+		homeLayout.getChildren().add(buttonHighScore);
+		homeScene = new Scene(homeLayout,1024,768);
+
+		StackPane singlePlayerLayout = new StackPane();
+		singlePlayerLayout.getChildren().add(buttonHomeScreen);
+		singlePlayerScene = new Scene(singlePlayerLayout, 1024,768);
+
+		primaryStage.setScene(homeScene);
+		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
