7efb6e25ec9f86cdc0f758562eb5f7a52365bc5a
Amar Parkash Singh <asin579@aucklanduni.ac.nz>
2018-03-29 14:16:32 +1300

    added all the needed scenes button back to main scene

--- PACMAN/Main.java
+++ PACMAN/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 	Button buttonMultiPlayer;
 	Button buttonHelp;
 	Button buttonHighScore;
-	Button buttonHomeScreen; //helper button
+	Button buttonHomeScreen,buttonHomeScreen1,buttonHomeScreen2,buttonHomeScreen3; //helper button
 
 	@Override
 	public void start(Stage primaryStage){
@@ -32,11 +32,20 @@ public class Main extends Application {
 		buttonSinglePlayer = new Button("Single Player");
 		buttonSinglePlayer.setOnAction(e -> primaryStage.setScene(singlePlayerScene));
 		buttonMultiPlayer = new Button("Multi-Player");
+		buttonMultiPlayer.setOnAction(e -> primaryStage.setScene(multiPlayerScene));
 		buttonHelp = new Button("Help");
+		buttonHelp.setOnAction(e -> primaryStage.setScene(helpScene));
 		buttonHighScore = new Button("High Score");
+		buttonHighScore.setOnAction(e -> primaryStage.setScene(highScoreScene));
 
 		buttonHomeScreen = new Button("Main menu");
 		buttonHomeScreen.setOnAction(e -> primaryStage.setScene(homeScene));
+		buttonHomeScreen1 = new Button("Main menu");
+		buttonHomeScreen1.setOnAction(e -> primaryStage.setScene(homeScene));
+		buttonHomeScreen2 = new Button("Main menu");
+		buttonHomeScreen2.setOnAction(e -> primaryStage.setScene(homeScene));
+		buttonHomeScreen3 = new Button("Main menu");
+		buttonHomeScreen3.setOnAction(e -> primaryStage.setScene(homeScene));
 
 		VBox homeLayout = new VBox(20);
 		homeLayout.getChildren().add(buttonSinglePlayer);
@@ -48,6 +57,18 @@ public class Main extends Application {
 		StackPane singlePlayerLayout = new StackPane();
 		singlePlayerLayout.getChildren().add(buttonHomeScreen);
 		singlePlayerScene = new Scene(singlePlayerLayout, 1024,768);
+
+		StackPane multiPlayerLayout = new StackPane();
+		multiPlayerLayout.getChildren().add(buttonHomeScreen1);
+		multiPlayerScene = new Scene(multiPlayerLayout, 1024,768);
+
+		StackPane helpLayout = new StackPane();
+		helpLayout.getChildren().add(buttonHomeScreen2);
+		helpScene = new Scene(helpLayout, 1024,768);
+
+		StackPane highScoreLayout = new StackPane();
+		highScoreLayout.getChildren().add(buttonHomeScreen3);
+		highScoreScene = new Scene(highScoreLayout, 1024,768);
 
 		primaryStage.setScene(homeScene);
 		primaryStage.show();
