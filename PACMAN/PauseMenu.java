ca8c41e7cc51b1e9a5024507928a13d9f4bf66ee
Amar Parkash Singh <asin579@aucklanduni.ac.nz>
2018-04-09 15:14:53 +1200

    pause menu update

---
+++ PACMAN/PauseMenu.java
@@ -0,0 +1,38 @@
+package PACMAN;
+
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class PauseMenu {
+
+	public static void pause(){
+
+		Stage pauseMenu = new Stage();
+		pauseMenu.initModality(Modality.APPLICATION_MODAL);
+
+		pauseMenu.setTitle("Pause Menu");
+		pauseMenu.setMinWidth(250);
+		pauseMenu.setMinHeight(70);
+
+		Button buttonResume = new Button("Resume");
+		buttonResume.setMinSize(100, 40);
+		buttonResume.setOnAction(e -> pauseMenu.close());
+		Button buttonExit = new Button("exit");
+		buttonExit.setMinSize(100, 40);
+		//buttonExit.setOnAction(e -> Main.start());
+
+		HBox pauseLayout = new HBox(20);
+		pauseLayout.setPadding(new Insets(15,15,15,15));
+		pauseLayout.setAlignment(Pos.CENTER);
+		pauseLayout.getChildren().addAll(buttonResume,buttonExit);
+
+		Scene paused = new Scene(pauseLayout);
+
+		pauseMenu.setScene(paused);
+		pauseMenu.showAndWait();
+	}
+
+}
